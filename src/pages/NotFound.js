e3b45520096651c4ab740bcf798bf0d0d71ba3e6
Sujay9550 <hardysujay@gmail.com>
2022-08-29 23:25:20 +0530

    Added image container

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,18 +1,22 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import notFoundImage from "../img/not-found.png";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import * as classes from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
     <div className="container mt-5 p-5 text-center text-secondary fw-bold">
       <div className="row">
         <div className="col-lg-12">
-          <LazyLoadImage
-            src={notFoundImage}
-            effect="blur"
-            height="400px"
-            className="img-fluid"
-          ></LazyLoadImage>
+          <div className={classes["notfound-image"]}>
+            <LazyLoadImage
+              src={notFoundImage}
+              effect="blur"
+              max-width="100%"
+              height="auto"
+              className="img-fluid"
+            ></LazyLoadImage>
+          </div>
           <p>The page you are looking for is not found!</p>
         </div>
       </div>
